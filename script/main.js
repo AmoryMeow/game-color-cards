9c5cd08e141d7c9db25750f5cff09ecb752984a9
Amory <garbage90@yandex.ru>
2020-08-20 18:46:35 +0300

    create field  not finished

--- script/main.js
+++ script/main.js
@@ -1,8 +1,8 @@
 const colors = ['hotpink', 'red', 'gold', 'green', 'purple','skyblue', 'cyan', 'coral', 'lightgreen', 'grey'];
 const currentField = [];
 let countCells = 20;
-let countCols = 5;
-let countRows = 4;
+let countCols = 2;
+let countRows = 2;
 let currentCouple = ''; //текущие открытые карточки. максимум 2
 let score = 0; //счет
 let step = 0; //ходы
@@ -17,7 +17,9 @@ const rowsDisplay = setting.querySelector('.setting__input_type_rows');
 const colsDisplay = setting.querySelector('.setting__input_type_cols');
 
 function closeAllCard() {
+  const cells = field.querySelectorAll('.field__cell');
   cells.forEach( function (cell) {
+
     closeCard(cell);
   });
 }
@@ -28,6 +30,7 @@ function closeCard(card) {
 }
 
 function openAllCard() {
+  const cells = field.querySelectorAll('.field__cell');
   cells.forEach( function (cell) {
     cell.classList.remove('field__cell_closed');
   });
@@ -53,7 +56,6 @@ function flipCard(evt) {
 
 /*проверить совпали ли карточки*/
 function checkMatch(currCard) {
-  console.dir(currentCouple.card);
   if (currentCouple === '') {
 
     currentCouple = currCard;
@@ -78,6 +80,10 @@ function checkMatch(currCard) {
 /*увеличить счет*/
 function scoreUp() {
   score += 2;
+  updateScore();
+}
+
+function updateScore() {
   scoreDisplay.textContent = '' + score + '/' + countCells;
 }
 
@@ -109,29 +115,64 @@ function initialColor() {
 
 function initialField() {
   colorArray = initialColor();
-
+  currentField.splice(0);
   for (let i = 0; i < countCells; i++) {
     currentField.push({card: cells[i], color: colorArray[i]})
   }
 }
 
+function createCell() {
+  const newCell = document.createElement('div');
+  newCell.classList.add('field__cell');
+  return newCell;
+}
+
+function createField() {
+  countRows = rowsDisplay.value;
+  countCols = colsDisplay.value;
+
+  field.style.gridTemplateColumns = `repeat(${countCols}, 80px)`;
+  field.style.gridTemplateRows = `repeat(${countRows}, 80px)`;
+
+  countCells = countRows * countCols;
+
+  for (let i=0;i<countCells;i++) {
+    const newCell = createCell();
+    field.append(newCell);
+  }
+
+}
+
+function clearField() {
+  field.children.forEach( (cell) => {
+    item.remove();
+  });
+}
+
 function start() {
+  clearField();
+  createField();
   initialField();
   closeAllCard();
+  updateScore();
   currentCouple = '';
   score = 0;
   scoreDisplay.textContent = '' + score + '/' + countCells;
   step = 0;
   stepDisplay.textContent = step;
-  rowsDisplay.value = countRows;
-  colsDisplay.value = countCols;
-
 }
 
-/****/
+function initialPage() {
+  rowsDisplay.value = countRows;
+  colsDisplay.value = countCols;
+  start();
+}
+
+/**обработчики элементов**/
 startGame.addEventListener('click', start);
 cells.forEach( (cell) => {
   cell.addEventListener('click', flipCard);
 });
 
-start();
+/*updage page */
+initialPage();
